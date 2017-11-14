87b920dc8f94f7d2593e73c532a9d2f11b422283
edgu100 <edgugold@gmail.com>
2017-11-15 10:27:43 +1300

    dsaw

---
+++ app/src/main/java/com/example/edgu1/angleseahospital/DB/drugsdetails.java
@@ -0,0 +1,19 @@
+package com.example.edgu1.angleseahospital.DB;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.edgu1.angleseahospital.R;
+
+
+/**
+ * Created by edgu1 on 2017/11/15.
+ */
+
+public class drugsdetails extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drugsdetails);
+    }
+}
---
+++ app/src/main/java/com/example/edgu1/angleseahospital/DrugsInformation.java
@@ -0,0 +1,17 @@
+package com.example.edgu1.angleseahospital;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by edgu1 on 2017/11/15.
+ */
+
+public class DrugsInformation extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drugsinfotmation);
+    }
+}
