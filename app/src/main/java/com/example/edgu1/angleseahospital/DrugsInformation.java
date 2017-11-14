1604a854d726e79b0f09ec65d46d6a379b74afff
edgu100 <edgugold@gmail.com>
2017-11-15 10:46:21 +1300

    dsaw

--- app/src/main/java/com/example/edgu1/angleseahospital/DrugsInformation.java
+++ app/src/main/java/com/example/edgu1/angleseahospital/DrugsInformation.java
@@ -2,7 +2,12 @@ package com.example.edgu1.angleseahospital;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 /**
  * Created by edgu1 on 2017/11/15.
@@ -10,8 +15,13 @@ import android.support.annotation.Nullable;
 
 public class DrugsInformation extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(  Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drugsinfotmation);
+
+        String[] drugs={};
+        ListAdapter drugsAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,drugs);
+        ListView drugsListView=(ListView)findViewById(R.id.DrugsDetails);
+        drugsListView.setAdapter(drugsAdapter);
     }
 }
