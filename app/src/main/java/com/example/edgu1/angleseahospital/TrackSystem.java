85f5edcfa8181790f332d38a98f3ad2b60189ca4
souun <32193812+souun@users.noreply.github.com>
2017-11-22 00:50:27 +1300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/edgu1/angleseahospital/TrackSystem.java
+++ app/src/main/java/com/example/edgu1/angleseahospital/TrackSystem.java
@@ -3,7 +3,13 @@ package com.example.edgu1.angleseahospital;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -24,13 +30,56 @@ import java.util.zip.Inflater;
  * Created by edgu1 on 2017/11/20.
  */
 
-public class TrackSystem extends Activity{
+public class TrackSystem extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+
     private SQLiteHelper dbHandler=null;
     private List<Map<String,String>> tracks;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tracksystem);
+
+
+        dl = (DrawerLayout)findViewById(R.id.ts);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        final NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.nav_home:
+                        Intent h = new Intent(TrackSystem.this, Mainpage.class);
+                        startActivity(h);
+                        break;
+                    case R.id.nav_patient:
+                        Intent p = new Intent(TrackSystem.this, PatientListPage.class);
+                        startActivity(p);
+                        break;
+                    case R.id.nav_drug:
+                        Intent d = new Intent(TrackSystem.this, DrugsInformation.class);
+                        startActivity(d);
+                        break;
+                    case R.id.nav_task:
+                        Intent t = new Intent(TrackSystem.this, CusTask.class);
+                        startActivity(t);
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+
         //PatientId
         String pid = String.valueOf(Parameters.pid);
         dbHandler = new SQLiteHelper(this);
@@ -101,6 +150,11 @@ public class TrackSystem extends Activity{
         Intent i=new Intent(this,Patient_info.class);
         startActivity(i);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
 }
