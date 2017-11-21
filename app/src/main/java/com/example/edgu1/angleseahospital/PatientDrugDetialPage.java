ee607a907e8810119a7e0d9e324ff396a681fc55
souun <32193812+souun@users.noreply.github.com>
2017-11-21 19:15:16 +1300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/edgu1/angleseahospital/PatientDrugDetialPage.java
+++ app/src/main/java/com/example/edgu1/angleseahospital/PatientDrugDetialPage.java
@@ -3,10 +3,21 @@ package com.example.edgu1.angleseahospital;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+import com.example.edgu1.angleseahospital.DB.Drug;
+import com.example.edgu1.angleseahospital.DB.Patient;
+import com.example.edgu1.angleseahospital.DB.PatientDrugs;
+import com.example.edgu1.angleseahospital.DB.SQLiteHelper;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by WX on 2017/11/21.
@@ -14,17 +25,68 @@ import android.widget.Toast;
 
 public class PatientDrugDetialPage extends Activity {
 
+    private SQLiteHelper dbHandler = null;
+    private Drug oldDrug = null;
+    private PatientDrugs oldPatientDrug = null;
+    private List<Map<String,String>> patientDrugs;
+    private List<Drug> drugs;
+    private int age=0;
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_drug_details);
+        dbHandler = new SQLiteHelper(this);
 
+        //Get Patient ID & Drug ID
         String ids = getIntent().getStringExtra("ids");
+        String[] idsarr = ids.split(",");
 
-        Toast.makeText(PatientDrugDetialPage.this, ids, Toast.LENGTH_LONG).show();
-        //Toast.makeText(PatientDrugDetialPage.this, pid, Toast.LENGTH_LONG).show();
+        String DrugID = String.valueOf(idsarr[0]);
+        String PatientID = String.valueOf(idsarr[1]);
+        dbHandler = new SQLiteHelper(this);
 
+        oldDrug =dbHandler.getDrugById(DrugID);
+        Patient p =dbHandler.getPatientById(Integer.parseInt(PatientID));
 
+        EditText pdd_dmg = (EditText)findViewById(R.id.pdd_dmg);
+        pdd_dmg.setText(oldDrug.getMilligrams());
+
+        EditText pdd_dml = (EditText)findViewById(R.id.pdd_dml);
+        pdd_dml.setText(oldDrug.getMilliliters());
+
+        EditText pdd_wight = (EditText)findViewById(R.id.pdd_wight);
+        pdd_wight.setText(p.getWeight()+"");
+
+        try {
+            age = TaskTimeTool.getAge(p.getBirthDay());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(age>16){
+            //textView3//pdd_getDosage/textView6/pdd_so/textView5/pdd_wight/textView7
+//            TextView textView3 = (TextView)findViewById(R.id.textView3);
+//            textView3.setVisibility(View.INVISIBLE);
+//            TextView pdd_getDosage = (TextView)findViewById(R.id.pdd_getDosage);
+//            pdd_getDosage.setVisibility(View.INVISIBLE);
+//            TextView textView6 = (TextView)findViewById(R.id.textView6);
+//            textView6.setVisibility(View.INVISIBLE);
+//            EditText pdd_so = (EditText)findViewById(R.id.pdd_so);
+//            pdd_so.setVisibility(View.INVISIBLE);
+//            TextView textView5 = (TextView)findViewById(R.id.textView5);
+//            textView5.setVisibility(View.INVISIBLE);
+//            EditText pddWight = (EditText)findViewById(R.id.pdd_wight);
+//            pddWight.setVisibility(View.INVISIBLE);
+//            TextView textView7 = (TextView)findViewById(R.id.textView7);
+//            textView7.setVisibility(View.INVISIBLE);
+            EditText pdd_so = (EditText)findViewById(R.id.pdd_so);
+            pdd_so.setEnabled(false);
+            EditText pddWight = (EditText)findViewById(R.id.pdd_wight);
+            pddWight.setEnabled(false);
+        }
     }
 
 
@@ -35,25 +97,26 @@ public class PatientDrugDetialPage extends Activity {
        EditText weight = (EditText)findViewById(R.id.pdd_wight);
        String getweight = weight.getText().toString();
 
+       EditText pdd_get_dosage = (EditText)findViewById(R.id.pdd_get_dosage);
+       String getdosage = weight.getText().toString();
+
+
        EditText pdd_dmg = (EditText)findViewById(R.id.pdd_dmg);
-       String getpdd_dmg = pdd_dmg.getText().toString();
 
        EditText pdd_dml = (EditText)findViewById(R.id.pdd_dml);
-       String getpdd_dml = pdd_dml.getText().toString();
-
+       //
        Double dosage;
        Double total_medication;
 
        if(getsOrder!=null&&!"".equals(getsOrder)&&getweight!=null&&!"".equals(getweight)){
-           try
-           {
+           try{
                // it means it is double & calculate dosage
                dosage = Double.parseDouble(getsOrder) * Double.parseDouble(getweight);
                TextView dosageText = (TextView) findViewById(R.id.pdd_getDosage);
                dosageText.setText(String.valueOf(dosage));
 
                //calculate Total Medication
-               total_medication = (dosage / Double.parseDouble(getpdd_dmg)) * Double.parseDouble(getpdd_dml);
+               total_medication = (dosage / Double.parseDouble(pdd_dmg.getText().toString())) * Double.parseDouble(pdd_dml.getText().toString());
                TextView total_medicationText = (TextView) findViewById(R.id.pdd_getTm);
                total_medicationText.setText(String.valueOf(total_medication));
 
@@ -61,12 +124,41 @@ public class PatientDrugDetialPage extends Activity {
                // this means it is not double
                e1.printStackTrace();
            }
-
-
        }
-
-
    }
+
+    class ProductListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return drugs.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.simple_drugs_list, null);
+            Drug drug = drugs.get(position);
+
+            EditText pdd_dmg = (EditText)findViewById(R.id.pdd_dmg);
+            EditText pdd_dml = (EditText)findViewById(R.id.pdd_dml);
+
+            pdd_dmg.setText(drug.getMilligrams());
+            pdd_dml.setText(drug.getMilliliters());
+
+
+            return view;
+        }
+    }
 
 //   public void pdd_cancle_onClick(View v){
 //
